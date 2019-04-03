c51cdc07a11b6bcb9d23733ba9b1ff5f44a31742
Drew Meier <darius.montez@gmail.com>
2019-04-03 08:11:40 -0400

    Serial port may be specified as 1st cmdline argument

--- src/XBeeTransmit.py
+++ src/XBeeTransmit.py
@@ -9,7 +9,13 @@ from builtins import input
 from digi.xbee.models.address import XBee16BitAddress, XBee64BitAddress
 
 
-COMport = 'COM9'
+import sys
+
+try:
+    COMport = sys.argv[1]
+except IndexError:
+    COMport = 'COM9'
+
 baud_rate = 9600
 remote_address = '0008'
 transmitDevice = XBeeDevice(COMport,baud_rate)
@@ -18,7 +24,7 @@ remoteDevice = RemoteXBeeDevice(transmitDevice, XBee64BitAddress.from_hex_string
 
 def main():
     transmitDevice.close()
-    
+
     print('transmitting to: ')
     print(remoteDevice.get_16bit_addr())
     cont = 'y'
@@ -26,7 +32,7 @@ def main():
         try:
             transmitDevice.open()
             instruction = getCommand()
-            
+
             #print(hex(instruction))
             sendInstructions(instruction)
         except Exception as e:
@@ -36,7 +42,7 @@ def main():
         transmitDevice.close()
         cont = input('Continue?')
     transmitDevice.close()
-    
+
 def getCommand():
     cmd = input("Specify a Command: ")
     dist = input("Distance: ")
@@ -44,6 +50,6 @@ def getCommand():
 
 def sendInstructions(data):
     transmitDevice.send_data(remoteDevice, data) #sends the data to the remote device
-    
+
 if __name__ == '__main__':
     main()
